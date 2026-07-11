407d70c1d0889dc3142cdd10cd4e8816e73a2a11
Ismael Moreira <ismaelgmoreirakt@gmail.com>
2026-07-11 12:48:46 -0800

    chore(scripts): introduce centralized script discovery type manifests
    Introduce 'FlagManifest', 'ScriptMetadata', and 'DiscoveryRegistry' type definitions within 'scripts/_internal/types.py' to establish a reusable, structural interface contract for automation utilities.

---
+++ scripts/_internal/types.py
@@ -0,0 +1,22 @@
+#     )
+#  ( /(                                   (           (
+#  )\())    )  (           (              )\       (  )\
+# ((_)\  ( /(  )(   `  )   )\ )     __ ((((_)(    ))\((_) (
+#  _((_) )(_))(()\  /(/(  (()/(    / /  )\ _ )\  /((_)_   )\
+# | || |((_)_  ((_)((_)_\  )(_))  / /   (_)_\(_)(_)) | | ((_)
+# | __ |/ _` || '_|| '_ \)| || | /_/     / _ \  / -_)| |/ _ \
+# |_||_|\__,_||_|  | .__/  \_, |        /_/ \_\ \___||_|\___/
+#                  |_|     |__/
+#
+# Copyright 2026 Ismael Moreira
+#
+# This file is distributed under the BSD 3-Clause License.
+#
+# See the LICENSE.txt file for more information.
+import typing
+
+FlagManifest = typing.Dict[str, typing.Tuple[typing.Optional[str], str]]
+ScriptMetadata = typing.TypedDict(
+    "ScriptMetadata", {"who_am_i": str, "flags": FlagManifest}
+)
+DiscoveryRegistry = typing.Dict[str, ScriptMetadata]
